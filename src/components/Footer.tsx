c458c8836590df8500698620dd7351c30537be12
Kesavarajan10 <120188691+Kesavarajan10@users.noreply.github.com>
2025-11-20 01:17:20 +0530

    fix5

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -191,9 +191,9 @@ const Footer = () => {
           <div className="border-t border-[#7C3AED] mt-12 mb-6"></div>
 
           {/* Bottom Section */}
-          <div className="flex flex-col md:flex-row justify-between items-center gap-[2rem] mb-6 pb-3 text-sm ">
+          <div className="flex flex-col md:flex-row justify-between items-center gap-[2rem] mb-6 pb-5 text-sm ">
             <div className="flex flex-wrap items-center gap-6 text-white px-[4px py-[12px]">
-              <span >© 2025 Copyright MuleCraft. All rights reserved.</span>
+              <span>© 2025 Copyright MuleCraft. All rights reserved.</span>
               <a href="#" className="hover:text-white transition-colors">
                 Privacy
               </a>
